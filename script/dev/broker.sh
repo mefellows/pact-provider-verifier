56bae795ce728cfa2c4b13289a4900e84ae6ffa3
Matt Fellows <matt.fellows@onegeek.com.au>
2019-11-10 13:47:05 +1100

    repro: demo basic auth credentials not used

--- script/dev/broker.sh
+++ script/dev/broker.sh
@@ -5,15 +5,11 @@ sleep 3
 export PACT_BROKER_PACTS_FOR_VERIFICATION_ENABLED=true
 
 bundle exec bin/pact-provider-verifier  \
+  https://test.pact.dius.com.au/pacts/provider/trip-service/consumer/billing-service/latest \
   --broker-username dXfltyFMgNOFZAxr8io9wJ37iUpY42M \
   --broker-password O5AIZWxelWbLvqMd8PkAVycBJh2Psyg1 \
-  --provider Bar \
-  --provider-version-tag pdev \
-  --pact-broker-base-url https://test.pact.dius.com.au \
-  -a 1.0.100 \
   --provider-base-url http://localhost:4567 \
   --provider-states-setup-url http://localhost:4567/provider-state \
-  --publish-verification-results \
   --format progress
 
 kill -2 $pid
